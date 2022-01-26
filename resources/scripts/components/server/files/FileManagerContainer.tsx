74943787697bee5569334397f333c7d345696013
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2022-01-25 18:51:46 -0800

    Testing width

--- resources/scripts/components/server/files/FileManagerContainer.tsx
+++ resources/scripts/components/server/files/FileManagerContainer.tsx
@@ -104,9 +104,9 @@ export default () => {
                 </ErrorBoundary>
 
                 <Formik initialValues={{ term: '' } as Values} onSubmit={searchFiles}>
-                    <Form css={tw`h-2/3 ml-10 w-5/6`}>
+                    <Form css={tw`ml-10 w-1/2`}>
                         <SearchWatcher />
-                        <Field css={tw`h-5/6`} as={InputWithRef} name={'term'} />
+                        <Field css={tw`h-2/3`} as={InputWithRef} name={'term'} />
                     </Form>
                 </Formik>
 
