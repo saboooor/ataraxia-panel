b9d8c2285a4d6e61355cd0c504eeea6abf1d75cb
Boy132 <Boy132@users.noreply.github.com>
2022-05-23 14:43:23 +0200

    separate stop and kill button

--- resources/scripts/components/server/PowerControls.tsx
+++ resources/scripts/components/server/PowerControls.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import tw from 'twin.macro';
 import Can from '@/components/elements/Can';
 import Button from '@/components/elements/Button';
-import StopOrKillButton from '@/components/server/StopOrKillButton';
 import { PowerAction } from '@/components/server/ServerConsole';
 import { ServerContext } from '@/state/server';
 
@@ -46,7 +45,29 @@ const PowerControls = () => {
                 </Button>
             </Can>
             <Can action={'control.stop'}>
-                <StopOrKillButton onPress={action => sendPowerCommand(action)}/>
+                <Button
+                    color={'red'}
+                    size={'xsmall'}
+                    css={tw`mr-2`}
+                    disabled={!status || status === 'offline'}
+                    onClick={e => {
+                        e.preventDefault();
+                        sendPowerCommand('stop');
+                    }}
+                >
+                    Stop
+                </Button>
+                <Button
+                    color={'red'}
+                    size={'xsmall'}
+                    disabled={!status || status === 'offline'}
+                    onClick={e => {
+                        e.preventDefault();
+                        sendPowerCommand('kill');
+                    }}
+                >
+                    Kill
+                </Button>
             </Can>
         </div>
     );
